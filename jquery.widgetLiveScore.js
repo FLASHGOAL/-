cc21b25a0c7a4e48eadde5fe13558b06925360de
FLASHGOAL <166710411+FLASHGOAL@users.noreply.github.com>
2024-04-12 17:51:00 +0330

    Update jquery.widgetLiveScore.js

--- jquery.widgetLiveScore.js
+++ jquery.widgetLiveScore.js
@@ -52,7 +52,7 @@
             // Get widget location
             var liveScoreLocation = $(this.element);
             // Construct the Live Channels Tab click
-            $('<a href="livechannels.html" class="titleWidget nav-tab">Live Channels</a>')
+            $('<a href="livechannels.html" class="titleWidget nav-tab">Live Channels</a>').prependTo($(liveScoreLocation));
             // Construct the Live Games Tab click
             $('<a href="#liveGame" class="titleWidget nav-tab">Live Games</a>').prependTo($(liveScoreLocation));
             // Construct the All Games Tab click
